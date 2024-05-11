feece4eec4faeb3844106e513d68a7acc632fb66
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-05-11 20:25:24 +0530

    update

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -7,10 +7,11 @@ const Contacts = () => {
       <div className='flex  items-center  justify-center text-white flex-col py-8 text-3xl'> Contact me</div>
       <div className=' flex flex-col justify-items-center items-center'>
 
-<form action="https://getform.io/f/eapdnoza" method='Post' className=' text-white flex flex-col focus:outline-none justify-center items-center  '>
+<form action="https://api.web3forms.com/submit" method='Post' className=' text-white flex flex-col focus:outline-none justify-center items-center  '>
+<input type="hidden" name="access_key" value="e632fd9c-4503-482a-bf46-9f3e5606d4fd"/>
 <input type="text" name='name' placeholder=' enter your name ' className='bg-transparent border-2 rounded-md py-2 w-2/3 md:w-96' />
 
-<input type="email" name='email' placeholder=' enter your email  ' className='bg-transparent py-2 border-2 rounded-md my-3 w-2/3 md:w-96'/>
+<input type="email" name='email' placeholder=' enter your email  ' className='bg-transparent py-2 border-2 rounded-md my-3 w-2/3 md:w-96' required/>
 <textarea name=" textarea" cols="50" rows="10" className=' bg-transparent py-2 border-2 rounded-md  w-2/3 md:w-96' placeholder='  enter your masseage'></textarea>
 <button className='bg-indigo-700  py-2 rounded-xl w-16 my-2'> let's talk </button>
 
