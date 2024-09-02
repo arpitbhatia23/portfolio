95e3db1cb31005b650083b43b9651f689755d088
Arpit <aurpitaurpit@gmail.com>
2024-09-02 10:19:29 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -43,7 +43,7 @@ function Dashboard() {
   return (
    
     <div name="Home" className=' md:min-h-screen min-h-screen  w-[100vw] bg-diagonal-gradient text-white  '>   
-    <div className='  min-w-[100vw ]sm:max-w-screen-lg mx-auto flex flex-col items-center  justify-end md:justify-end md:items-center   h-60   px-3  text-3xl space-x-2 ' >
+    <div className='  min-w-[100vw ]sm:max-w-screen-lg mx-auto flex flex-col items-center  justify-end md:justify-end md:items-center   h-60   px-3  text-2xl sm:text-3xl space-x-2 ' >
       
 
     <p>I'm a </p> 
